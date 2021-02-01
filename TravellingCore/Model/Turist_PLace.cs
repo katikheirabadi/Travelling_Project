670a1eba3432aa3e2269102626798eabf2360e54
ASUS <kati.kheirabadi@gmail.com>
2021-02-01 17:31:31 +0330

    aaa

--- TravellingCore/Model/Turist_PLace.cs
+++ TravellingCore/Model/Turist_PLace.cs
@@ -14,6 +14,6 @@ namespace TravellingCore.Model
         public string Description { get; set; }
         public int Visit { get; set; }
         public List<Comment> Comments { get; set; }
-        public List<Turist_Place> Rates { get; set; }
+        public List<Rate> Rates { get; set; }
     }
 }
